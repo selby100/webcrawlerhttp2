01bc34b3e1e33b185454c0ef6868641caae822a8
Robert Douglas <Robert.Douglas@manpower.co.uk>
2023-05-11 11:38:41 +0100

    crawl page recursive working 2

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { crawlPage } = require ('./crawl.js')
 
-function main()
+async function main()
 {
     if (process.argv.length < 3) {
         console.log("no website provided")
@@ -15,7 +15,7 @@ function main()
     const baseURL = process.argv[2]
 
     console.log(`starting crawl of ${baseURL}`)
-    const pages = crawlPage(baseURL, baseURL, {})
+    const pages = await crawlPage(baseURL, baseURL, {})
 
     for (const page of Object.entries(pages)) {
         console.log(page)
