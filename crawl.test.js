3b2e84777d5cb1b50af83bb63f3908d316779043
Robert Douglas <Robert.Douglas@manpower.co.uk>
2023-05-11 13:04:52 +0100

    printReport finished

--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,4 @@
 const {normalizeURL, getURLsFromHTML} = require('./crawl.js')
-//const {normalizeURL} = require('./crawl.js')
 const {test, expect} = require('@jest/globals')
 
 test('normalizeURL strip protocol', () => {
